8582b6deceae43242634a339b3adf0b70d79bb72
copydataai <copydataai@gmail.com>
2021-08-22 17:01:53 -0400

    Add extract simple from title, copete, epigrapho, body.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup as bs
+from IPython.display import Image
 from requests import get
+
+def take_img(link):
+    img = get(link)
 
 def take_link(section, url):
     """get link, and process other links with title."""
@@ -13,6 +17,19 @@ def take_link(section, url):
         return links
     return links
 
+def new(link):
+    """Extract from link: title,"""
+    link = get(link)
+    soup = bs(link.text, 'html.parser')
+    # Extract title
+    title = soup.find('h1', attrs={'data-testid': 'headline'})
+    copete = soup.find('p')
+    epigrapho = soup.find('figcaption', attrs={'class': 'css-13o4bnb e18f7pbr0'})
+    body = soup.find_all('p', attrs={'class': 'css-axufdj evys1bk0'})
+    bodys = [line.get_text() for line in body]
+    if not body:
+        body = soup.find_all('p', attrs={'class': 'css-iynevi evys1bk0'})
+        bodys = [line.get_text() for line in body]
 
 
 def main():
@@ -23,7 +40,10 @@ def main():
     links = [(link.a.get_text(), url+link.a.get('href')) for link in sections]
     for link in links:
         print(link[0])
-        print(take_link(link[1], url))
+        links = take_link(link[1], url)
+        for link in links:
+            new(link[1])
+
 
 if __name__ == '__main__':
     main()
